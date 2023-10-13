17f8e2fd650a1b9564c04c710be4c360e1149447
halaBarid <baridhala@gmail.com>
2023-10-13 16:25:20 +0100

    fill some pages

--- menu.js
+++ menu.js
@@ -1,71 +1,32 @@
 // Menu page
 
-const coffeeButtonMenu = document.querySelector(".coffee")
-const dessertButtonMenu = document.querySelector(".desserts")
-const drinksButtonMenu = document.querySelector(".drinks")
-const smoothiesButtonMenu = document.querySelector(".smoothies")
+const buttons = document.querySelectorAll("button")
 
-const coffeeMenu = document.querySelector(".coffee-menu")
-const dessertMenu = document.querySelector(".dessert-menu")
-const drinksMenu = document.querySelector(".drinks-menu")
-const smoothiesMenu = document.querySelector(".smoothies-menu")
-
-const buttons = document.querySelectorAll(".button")
-console.log(buttons)
-
-dessertMenu.style.display = "none"
-drinksMenu.style.display = "none"
-smoothiesMenu.style.display = "none"
-
-coffeeButtonMenu.addEventListener("click", () => {
-    coffeeMenu.style.display= "block"
-    dessertMenu.style.display = "none"
-    drinksMenu.style.display = "none"
-    smoothiesMenu.style.display = "none"
+const menuItems = [
+    document.querySelector(".coffee-menu"),
+    document.querySelector(".dessert-menu"),
+    document.querySelector(".drinks-menu"),
+    document.querySelector(".smoothies-menu")
+]
 
 
-    coffeeButtonMenu.classList.add("active")
-    dessertButtonMenu.classList.remove("active")
-    drinksButtonMenu.classList.remove("active")
-    smoothiesButtonMenu.classList.remove("active")
-})
+buttons.forEach( (element,index) => {
+    element.addEventListener("click", () => {
 
-dessertButtonMenu.addEventListener("click", () => {
-    coffeeMenu.style.display= "none"
-    dessertMenu.style.display= "block"
-    drinksMenu.style.display = "none"
-    smoothiesMenu.style.display = "none"
+        // Remove "active" class from all buttons
+        buttons.forEach(element => {
+            element.classList.remove("active")
+        });
 
+        // Add "active" class to the clicked button
+        element.classList.add("active") 
 
-    dessertButtonMenu.classList.add("active")
-    coffeeButtonMenu.classList.remove("active")
-    drinksButtonMenu.classList.remove("active")
-    smoothiesButtonMenu.classList.remove("active")
-})
+        // Hide all menu items
+        menuItems.forEach(menuItem => {
+            menuItem.style.display = "none"
+        });
 
-drinksButtonMenu.addEventListener("click", () => {
-    coffeeMenu.style.display= "none"
-    dessertMenu.style.display= "none"
-    drinksMenu.style.display= "block"
-    smoothiesMenu.style.display = "none"
-
-
-    drinksButtonMenu.classList.add("active")
-    dessertButtonMenu.classList.remove("active")
-    coffeeButtonMenu.classList.remove("active")
-    smoothiesButtonMenu.classList.remove("active")
-
-})
-
-smoothiesButtonMenu.addEventListener("click", () => {
-    coffeeMenu.style.display= "none"
-    dessertMenu.style.display= "none"
-    drinksMenu.style.display= "none"
-    smoothiesMenu.style.display= "block"
-
-
-    smoothiesButtonMenu.classList.add("active") 
-    dessertButtonMenu.classList.remove("active")
-    coffeeButtonMenu.classList.remove("active")
-    drinksButtonMenu.classList.remove("active")    
-})
+        // Show the selected menu item based on the index
+        menuItems[index].style.display = "block"
+    })
+});
